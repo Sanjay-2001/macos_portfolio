c777e86211af7726bcb58ff4f164c8b7726492f7
Sanjay-2001 <sanjaydutta976@gmail.com>
2025-12-21 15:49:52 +0530

    FEAT: hoc window wrapper with close and draggable and terminal window added

---
+++ src/components/WindowControls.tsx
@@ -0,0 +1,16 @@
+import type { WindowKey } from "../constants";
+import useWindowStore from "../store/window";
+
+const WindowControls = ({ target }: { target: WindowKey }) => {
+  const { closeWindow } = useWindowStore();
+
+  return (
+    <div id="window-controls">
+      <div className="close" onClick={() => closeWindow(target)} />
+      <div className="minimize" />
+      <div className="maximize" />
+    </div>
+  );
+};
+
+export default WindowControls;
