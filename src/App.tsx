86498bbff887ebc481eb7e88092a4546ea7ee582
Sanjay-2001 <sanjaydutta976@gmail.com>
2025-12-23 11:44:14 +0530

    FEAT: photos window added

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import {
   Contact,
   Finder,
   Image,
+  Photos,
   Resume,
   Safari,
   Terminal,
@@ -27,6 +28,7 @@ const App = () => {
       <Image />
       <Contact />
       <Home />
+      <Photos />
     </main>
   );
 };
