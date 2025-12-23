da2254d3cc7fb4657dd53d606d2e2eba73a10987
Sanjay-2001 <sanjaydutta976@gmail.com>
2025-12-23 10:46:57 +0530

    FEAT: image window added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Draggable } from "gsap/Draggable";
 import gsap from "gsap";
 import { Dock, Navbar, Welcome } from "./components";
-import { Finder, Resume, Safari, Terminal, Text } from "./windows";
+import { Finder, Image, Resume, Safari, Terminal, Text } from "./windows";
 
 gsap.registerPlugin(Draggable);
 
@@ -16,6 +16,7 @@ const App = () => {
       <Resume />
       <Finder />
       <Text />
+      <Image />
     </main>
   );
 };
